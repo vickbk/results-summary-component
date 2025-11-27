0a7f5fc0a38cd9bd023af534faa180a57851bbdc
vickbk <vickbake8@gmail.com>
2025-11-28 01:57:45 +0200

    Setting preview colors

--- src/components/result-card/preview/PreviewDetails.tsx
+++ src/components/result-card/preview/PreviewDetails.tsx
@@ -1,8 +1,8 @@
 export const PreviewDetails = () => {
   return (
     <p className="text-sm">
-      <span className="block text-xl">Great</span> You scored higher than 65% of
-      the people who have taken these tests.
+      <span className="block text-xl mb-2">Great</span> You scored higher than
+      65% of the people who have taken these tests.
     </p>
   );
 };
--- src/components/result-card/preview/PreviewScore.tsx
+++ src/components/result-card/preview/PreviewScore.tsx
@@ -1,6 +1,6 @@
 export const PreviewScore = ({ score }: { score: number }) => {
   return (
-    <div className="px-8 justify-self-center grid items-center aspect-square rounded-full pale-blue">
+    <div className="px-8 justify-self-center grid items-center aspect-square rounded-full pale-blue preview__score">
       <div>
         <span className="block text-5xl font-bold">{score.toFixed()}</span>
         <span className="text-xs"> of 100</span>
