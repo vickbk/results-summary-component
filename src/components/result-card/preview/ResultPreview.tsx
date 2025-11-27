a859e3b169118c6b48d002676f719938d5eb0412
vickbk <vickbake8@gmail.com>
2025-11-28 01:41:18 +0200

    adding bottom rounded corners for preview section

--- src/components/result-card/preview/ResultPreview.tsx
+++ src/components/result-card/preview/ResultPreview.tsx
@@ -3,7 +3,7 @@ import { PreviewScore } from "./PreviewScore";
 
 export const ResultPreview = ({ score }: { score: number }) => {
   return (
-    <section className="grid gap-4 p-8 violet-blue c-white">
+    <section className="grid gap-4 p-8 violet-blue c-white rounded-b-4xl">
       <h3>Your Result</h3>
       <PreviewScore score={score} />
       <PreviewDetails />
