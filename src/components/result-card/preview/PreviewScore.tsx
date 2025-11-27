89d3c2ff11770b74d189a27cba2bb6e338371fca
vickbk <vickbake8@gmail.com>
2025-11-28 01:20:21 +0200

    initial colors for result preview

--- src/components/result-card/preview/PreviewScore.tsx
+++ src/components/result-card/preview/PreviewScore.tsx
@@ -1,8 +1,10 @@
 export const PreviewScore = ({ score }: { score: number }) => {
   return (
-    <div className="px-4 justify-self-center grid place-items-center aspect-square">
-      <span className="block text-4xl">{score.toFixed()}</span>
-      <span className="text-xs"> of 100</span>
+    <div className="px-8 justify-self-center grid items-center aspect-square rounded-full pale-blue">
+      <div>
+        <span className="block text-5xl font-bold">{score.toFixed()}</span>
+        <span className="text-xs"> of 100</span>
+      </div>
     </div>
   );
 };
--- src/components/result-card/preview/ResultPreview.tsx
+++ src/components/result-card/preview/ResultPreview.tsx
@@ -3,7 +3,7 @@ import { PreviewScore } from "./PreviewScore";
 
 export const ResultPreview = ({ score }: { score: number }) => {
   return (
-    <section className="grid gap-4 p-8">
+    <section className="grid gap-4 p-8 violet-blue c-white">
       <h3>Your Result</h3>
       <PreviewScore score={score} />
       <PreviewDetails />
