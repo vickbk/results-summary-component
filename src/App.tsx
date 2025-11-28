082c6dc6d5235af80c29342c7b5983befeba41ba
vickbk <vickbake8@gmail.com>
2025-11-28 02:02:57 +0200

    adding h1 to the page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { ResultCard } from "./components/result-card/ResultCard";
 function App() {
   return (
     <main className="grid place-items-center min-h-screen">
+      <h1 className="sr-only">A page containing a result summary component.</h1>
       <ResultCard />
     </main>
   );
